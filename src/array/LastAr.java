214bd1421b7c3b66d96018b3bfdc4c626e4dcb6c
digvijay parade <digvijayparde@gmail.com>
2026-07-25 16:58:33 +0530

    //

---
+++ src/array/LastAr.java
@@ -0,0 +1,21 @@
+package array;
+
+import java.util.*;
+
+public class LastAr {
+	
+	
+	public static void main(String[] args) {
+		
+		int [] ar = new int [5];
+		ar[0] = 3;
+		meth(ar);
+	}
+
+
+public static void meth(int[] num) {
+	
+	System.out.println(Arrays.toString(num));
+	
+}
+}
---
+++ src/commandlineargs/Cmd.java
@@ -0,0 +1,5 @@
+package commandlineargs;
+
+public class Cmd {
+
+}
