40569b59287b39fa79673ce4466026c56bd32cd8
digvijay parade <digvijayparde@gmail.com>
2026-07-26 13:20:34 +0530

    [

--- src/ArgumentDemo.java
+++ src/ArgumentDemo.java
@@ -1,12 +1,10 @@
 public class ArgumentDemo {
-    // Renamed "args" to "bananas" - this is 100% legal
+    
     public static void main(String[] bananas) {
         
-        // Check if the user passed any text in the terminal
         if (bananas.length > 0) {
-            System.out.println("🎉 Arguments received!");
+            System.out.println("Arguments received!");
             
-            // Loop through and print every item in our array
             for (int i = 0; i < bananas.length; i++) {
                 System.out.println("Argument " + i + ": " + bananas[i]);
             }
--- src/array/Arr.java
+++ src/array/Arr.java
@@ -2,41 +2,62 @@ package array;
 
 import java.util.*;
 
-public class Arr {
-
+public class Arr{
 	
-	int id;
-	int price;
-	String name;
-	
-	public Arr (int id,int price,String name) {
+	String name ;
+	int id ;
+	double price ;
+	public Arr(String name ,
+	int id ,
+	double price ) {
 		
-		this.id = id;
-		this.price = price;
-		this.name = name;
-		
-		
+		this. name = name;
+		this. id = id ;
+		this. price  = price;
 	}
-	
 	public static void main(String[] args) {
 		
-		Arr [] productdetails = new Arr[5];
+		Arr[] detail = new Arr[5];
+		detail[0] = new Arr("Iphone",38,75000.00);
 		
-		productdetails[0] = new Arr(29,78500,"Iphone");
-	
+		System.out.println(detail[0]);
+		detail[1]=show(new Arr("Samsung",97,40000.00));
+		show(detail[0]);
 		
-		productInformation(productdetails[0]);
+		ArrayList <Arr> data = new ArrayList<> ();
+		data.add(show(new Arr("Xiaomi",69,20000.00)));
+		data.add(show(new Arr("Samsung",97,40000.00)));
+		for(Arr values : data) {
+			
+			System.out.println(values);
+		}
+		for(int i = 0 ; i < data.size() ;i++) {
+			
+			if (data.get(i).name.equals("Xiaomi")) {
+		        
+		        System.out.println("Match found at index " + i + ":");
+		        System.out.println(data.get(i).name);
+		        System.out.println(data.get(i).id);
+		        System.out.println(data.get(i).price);
+			}
+			
+		}
+		for (int i = 0 ;i < detail.length;i++) {
+			
+			Arr target = new Arr("Samsung",97,40000.00) ;
+			if (target.equals(detail[i])) {
+				
+				System.out.println(i);
+			}
+		}
 		
-		ArrayList <Arr> info = new ArrayList<> ();
-		
-		info.add(new Arr(29,78500,"Iphone"));
 	}
-	public static void productInformation(Arr product) {
+	public static Arr show(Arr product) {
 		
+		System.out.println(product.name);
 		System.out.println(product.id);
 		System.out.println(product.price);
-		System.out.println(product.name);
 		
+		return product;
 	}
-	
 }
