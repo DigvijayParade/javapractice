f47cdcd6440ab39e45a19551697e059b397ba5bd
digvijay parade <digvijayparde@gmail.com>
2026-07-27 14:34:35 +0530

    22

--- src/inheritance/InDemo.java
+++ src/inheritance/InDemo.java
@@ -1,5 +1,31 @@
 package inheritance;
 
-public class InDemo {
+class Main{
 
+	void what() {
+		
+		int a;
+		System.out.println("this is a Main class method");
+	}
+	
+}class Demo{
+	
+	void how() {
+		
+		System.out.println("this is a Demo class method");
+	}
+	
 }
+public class InDemo {
+	
+	public static void main(String[] args) {
+			
+			here(new Main());
+		}
+	public static void here(Main meth) {
+		
+		meth.what();
+		
+	}
+	}
+
