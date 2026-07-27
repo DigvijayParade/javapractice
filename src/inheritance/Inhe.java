0a2b76f46cd0399c0a7c24675c573cfb83d44a95
digvijay parade <digvijayparade@gmail.com>
2026-07-27 23:51:13 +0530

    git checking 3

--- src/inheritance/Inhe.java
+++ src/inheritance/Inhe.java
@@ -1,8 +1,39 @@
 package inheritance;
 
-public class Inhe {
+
+interface one{
+	
+	default void meth0() {
+		
+		System.out.println("this is a interface ones method");
+	}
+}
+
+interface Two{
+	
+	void meth();
+	
+	static void meth1() {
+		
+		System.out.println("This is a interface twos normal method");
+	}
+}
+public class Inhe implements Two,one{
 
 	public static void main(String[] args) {
 		
+		Inhe obj = new Inhe();
+		obj.meth();
+		Two.meth1();
+		obj.meth0();
+	}
+	public void meth() {
+		
+		System.out.println("this is intefaces overriden abstract method");
+	}
+	
+	public void meth0() {
+		
+		System.out.println("interface ones overriden method");
 	}
 }
