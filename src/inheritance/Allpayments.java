fa08dce873feda7aa50af6aa5c3e6df629d33f11
digvijay parade <digvijayparade@gmail.com>
2026-07-31 01:03:32 +0530

    im gonna use this class in other seprate class file

--- src/inheritance/Allpayments.java
+++ src/inheritance/Allpayments.java
@@ -2,4 +2,28 @@ package inheritance;
 
 public class Allpayments {
 
+	String username;
+	int userid;
+	public Allpayments(String username ,int userid){
+		
+		this.username = username;
+		this.userid = userid;
+	}
+	
+	public void netbank(int amount) {
+		
+		System.out.println(username+" "+"paid"+" "+amount+" "+"through the netbanking");
+	}
+	public void debitcard(int amount) {
+		
+		System.out.println(username+" "+"paid"+" "+amount+" "+"through the debitcard");
+	}
+	public void creditcard(int amount) {
+		
+		System.out.println(username+" "+"paid"+" "+amount+" "+"through the creditcard");
+	}
+	public void upi(int amount) {
+		
+		System.out.println(username+" "+"paid"+" "+amount+" "+"through the creditcard");
+	}
 }
