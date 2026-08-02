38c467f453cc77b94b6f629d41ce08d828a1ce5d
digvijay parade <digvijayparade@gmail.com>
2026-08-02 18:46:31 +0530

    /

--- src/inheritance/BankApp.java
+++ src/inheritance/BankApp.java
@@ -1,78 +1,88 @@
 package inheritance;
 
-class BankAcc{
-	
-	String accountHolder;
+class BankAcc {
+    String accountHolder;
+    double balance;
 
-	double balance;
-	
-	public BankAcc(String accountHolder , double balance) {
-		
-		this.accountHolder =  accountHolder;
-		this. balance = balance;
-	}
-	
-	void depoait(double amount) {
-		
-		if (amount > 0) {
-			
-			System.out.println(amount+" "+"deposited succesfully");
-		}else {
-			
-			System.out.println("Invalid amount");
-		}
-	}
-	void withdraw(double amount) {
-		
-		if(amount <= amount) {
-			
-			System.out.println((amount - amount)+" "+"withdrwed from your account");
-		}
-		else {
-			
-			System.out.println("you dont have enought balance to make this withdraw");
-		}
-	}
+    public BankAcc(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited $" + amount + ". Balance: $" + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew $" + amount + ". Balance: $" + balance);
+        } else {
+            System.out.println("Insufficient funds or invalid amount.");
+        }
+    }
 }
 
-class SavingAcc extends BankAcc{
-	
-	double minBalance = 500.0;
-	
-	public SavingAcc(String accountHolder , double balance){
-		
-		super(accountHolder ,balance);
-	}
-	
-	public static void main(String[] args) {
-		
-		SavingAcc obj = new SavingAcc("Yeda pandu",800.00);
-		obj.withdraw(400.00);
-	}
-	
-	@Override
-	void withdraw(double amount) {
-		
-		if(amount <= 0) {
-			
-			System.out.println("Withdrawal amount must be positive!");
-		}
-		else if((balance - amount) >= minBalance) {
-			
-			System.out.println("Withdrew $"+" "+amount+" "+" New Balance: $"+" "+balance);
-		}
-		else if(balance >= amount) {
-			
-			 	System.out.println("\"Transaction Failed! Savings account must maintain at least $500 balance.\"");
-		}
-		else {
-			
-			System.out.println("\"Transaction Failed! Insufficient total funds.");
-		}
-		
-	}
+class SavingAcc extends BankAcc {
+    double minBalance = 500.0;
+
+    public SavingAcc(String accountHolder, double balance) {
+        super(accountHolder, balance);
+    }
+
+    @Override
+    void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Withdrawal amount must be positive!");
+        } else if ((balance - amount) >= minBalance) {
+            balance -= amount;
+            System.out.println("Withdrew $" + amount + ". New Balance: $" + balance);
+        } else if (balance >= amount) {
+            System.out.println("Transaction Failed! Savings account must maintain at least $500 balance.");
+        } else {
+            System.out.println("Transaction Failed! Insufficient total funds.");
+        }
+    }
 }
 
-//public class SavingAcc {
-//
-//}
+class CurrentAcc extends BankAcc {
+    double overdraftLimit = 1000.0;
+
+    public CurrentAcc(String accountHolder, double balance) {
+        super(accountHolder, balance);
+    }
+
+    @Override
+    void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Withdrawal amount must be positive!");
+        } else if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew $" + amount + ". Remaining Balance: $" + balance);
+        } else if (amount <= (balance + overdraftLimit)) {
+            balance -= amount;
+            System.out.println("Overdraft Used! Withdrew $" + amount + ". Current Balance: $" + balance);
+        } else {
+            System.out.println("Transaction Denied! Exceeds maximum overdraft limit of $1000.");
+        }
+    }
+}
+
+public class BankApp {
+    public static void main(String[] args) {
+        System.out.println("--- Savings Account Test ---");
+        SavingAcc alex = new SavingAcc("Alex", 1200.0);
+        alex.withdraw(800.0);
+        alex.withdraw(500.0);
+
+        System.out.println("\n--- Current Account Test ---");
+        CurrentAcc sam = new CurrentAcc("Sam", 300.0);
+        sam.withdraw(800.0);
+        sam.withdraw(1000.0);
+    }
+}
