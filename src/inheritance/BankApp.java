710233b5b8b4daa93b1e5f9b5e609b5582e7d250
digvijay parade <digvijayparade@gmail.com>
2026-08-02 17:08:32 +0530

    //

---
+++ src/inheritance/BankApp.java
@@ -0,0 +1,23 @@
+package inheritance;
+
+class BankAcc{
+	
+	String accountHolder;
+
+	double balance;
+	
+	public BankAcc(String accountHolder , double balance) {
+		
+		this.accountHolder =  accountHolder;
+		this. balance = balance;
+	}
+	
+	void depoait(double amount) {
+		
+		
+	}
+}
+
+public class BankApp {
+
+}
