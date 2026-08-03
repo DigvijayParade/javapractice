17c211f12dc5bf050a8fbd74175af32e3f597188
digvijay parade <digvijayparade@gmail.com>
2026-08-03 14:58:12 +0530

    //

--- src/Interface/BattleArena.java
+++ src/Interface/BattleArena.java
@@ -1,3 +1,69 @@
 package Interface;
 
+interface Hero{
+	
+	void attack() ;
+	void useSpecialAbility() ;
+}
+
+class BaseHero implements Hero{
+	
+	String name;
+	int health;
+	
+	public BaseHero(String name , int health) {
+		
+		this.name = name ;
+		this.health = health ;
+	}
+	
+	@Override
+	public void attack() {
+		
+		System.out.println(name+" "+"swings a basic weapon for 10 Damage");
+	}
+	
+	@Override
+	public void useSpecialAbility() {
+		
+		System.out.println(name+" "+"use basic hero kills");
+	}
+}
+
+class Mage extends BaseHero{
+	
+	int mana = 50;
+	
+	public Mage(String name , int health){
+		
+		super(name, health);
+	}
+	
+	@Override 
+	public void useSpecialAbility() {
+		
+		if (mana <= 30) {
+			
+			mana -= 30;
+			System.out.println(name+" "+"casts fireball Remainig mana : "+" "+mana);
+		}
+		else {
+			
+			System.out.println(name+" tries to cast Fireball, but doesn't have enough mana!");
+		}
+	}
+}
+
+class Archer extends BaseHero{
+	
+	int ammo = 2;
+	public Archer(String name , int health) {
+		
+		super(name,health);
+	}
+	
+	@Override
+	public void useSpecialAbility() {}
+}
+
 public class BattleArena{}
