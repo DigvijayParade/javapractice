5cd51f38f77b6dc669a5ad0f3f0cb522e941e721
digvijay parade <digvijayparade@gmail.com>
2026-08-06 00:44:08 +0530

    //

---
+++ src/interfaces/Try.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public interface Try {
+
+	public static void main(String[] args) {
+		
+		what();
+	}
+	public static void what() {
+		
+		System.out.println("i didnt know an interface can have a main mwrhod");
+	}
+}
