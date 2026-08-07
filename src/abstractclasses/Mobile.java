e12f30f02a8c859469932bab23d3255269fe9a66
digvijay parade <digvijayparade@gmail.com>
2026-08-07 14:41:52 +0530

    //

--- src/abstractclasses/Mobile.java
+++ src/abstractclasses/Mobile.java
@@ -2,8 +2,26 @@ package abstractclasses;
 
 public interface Mobile {
 
-	void charge(int batterylevel);
-	void call(boolean isCalling);
+	default void charge(int batterylevel) {
+		if (batterylevel > 100 || batterylevel < 0) {
+			System.out.println("Battery is damaged");
+		}
+		else if (batterylevel < 10) {
+			System.out.println("Immediately charge the phone");
+		}
+		else {
+			System.out.println("Phone does not need to be charged right now");
+		}
+	}
+
+	default void call(boolean isCalling) {
+		if (isCalling) {
+			System.out.println("pickup the phone");
+		}
+		else {		
+			System.out.println("no one is calling");
+		}
+	}
+
 	void playGames();
-	
 }
