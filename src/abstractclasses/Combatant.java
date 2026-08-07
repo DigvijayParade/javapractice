3cb7e7ede55aaecee9778db15d996bd654cb4bb5
digvijay parade <digvijayparade@gmail.com>
2026-08-07 22:33:35 +0530

    //

--- src/abstractclasses/Combatant.java
+++ src/abstractclasses/Combatant.java
@@ -7,3 +7,34 @@ public interface Combatant {
 	void usespecialability();
 
 }
+
+abstract class GameCharacter implements Combatant{
+	
+	String name ;
+	int health ;
+	
+	public GameCharacter(String name ,int health) {
+		
+		this.name = name ;
+		this. health = health ;
+		
+		System.out.println("Character Name : "+name);
+		System.out.println(name+"'s"+" "+health);
+	}
+}
+
+abstract class Hero extends GameCharacter{
+	
+	int stamina ;
+	
+	public Hero(String name ,int health,int stamina) {
+		
+		super(name , health);
+		this.stamina = stamina ;
+		
+		System.out.println("Hero Name : "+name);
+		System.out.println(name+"'s"+" "+health);
+		System.out.println(name+"'s"+" "+stamina);
+		
+	}
+}
