288cd8ffbcc53249e07f86289fab53e20c4337b2
digvijay parade <digvijayparade@gmail.com>
2026-08-12 00:03:29 +0530

    //

--- src/keywords/this_super_final/SuperDemo.java
+++ src/keywords/this_super_final/SuperDemo.java
@@ -8,31 +8,51 @@ class Unsuper{
 		this.laugh = laugh ;
 	}
 	
-	void m1() {System.out.println("Method 1 of paremt class");}
+	void m10() {System.out.println("Method 1 of paremt class");}
 	void m2() {System.out.println("Method 2 of parent class");}
 }
 
-public class SuperDemo extends Unsuper{
-
-	public SuperDemo (String laugh) {
+interface SuperDuper{
+	
+	//well whats the point of variable cause they are always final no matter from where we access interface vars :)
+	default void m3() {
 		
-		super(laugh);
+		System.out.println("Interface method");
 	}
+}
+
+public class SuperDemo extends Unsuper implements SuperDuper{
+
+	String laugh ;
+	public SuperDemo(String childLaugh, String parentLaugh) {
+        super(parentLaugh);        
+        this.laugh = childLaugh;  
+    }
 	
 	public static void main(String[] args) {
 		
-	SuperDemo obj = new SuperDemo("No Laugh its super now");
+	SuperDemo obj = new SuperDemo("No Laugh its super now", "laugh its unsuper");
 	Unsuper obj1 = new Unsuper("This is just unsuper obj");
-	obj.m1();
+	obj.m10();
 	obj.m2();
+	obj.m3();
 	
 	}
 	
 	
-	public void m1() {
+	public void m10() {
 		
-//		System.out.println("Overrided m1 method from the parent class");
+		System.out.println("1st using the overrided method logic than the parent class logic ");
+		System.out.println("parents laugh ,"+this.laugh);
+		System.out.println("childs laugh ,"+super.laugh);
+		super.m10();
+	}
+	
+	@Override
+	public void m3() {
 		
-		super.m1();
+		System.out.println("Overriden interface method");
+		System.out.println("Now for the original method");
+		SuperDuper.super.m3();
 	}
 }
