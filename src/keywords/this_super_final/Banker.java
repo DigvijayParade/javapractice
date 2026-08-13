d278523645025fd5125e8b81622c9c95325738ff
digvijay parade <digvijayparade@gmail.com>
2026-08-13 15:33:52 +0530

    //

--- src/keywords/this_super_final/Banker.java
+++ src/keywords/this_super_final/Banker.java
@@ -4,7 +4,7 @@ package keywords.this_super_final;
 	 
 	 String accHolder ;
 	 int balance ;
-	 final int BANK1002 ;
+	 final int routingNum = 1001;
 	 
 	 public BankAcc(String accHolder ,int balance) {
 		 
@@ -12,13 +12,104 @@ package keywords.this_super_final;
 		 this.balance = balance ;
 	 }
 	 
-	 public BankAcc(final int BANK1002) {
+	 public BankAcc(String accHolder) {
 		 
-		 this.BANK1002 = BANK1002 ;
+		    this(accHolder, 100); 
+		}
+	 
+	 void displayInfo() {
+		 
+		 System.out.println("Account Holder Name : "+accHolder);
+		 System.out.println("Balance : "+balance);
+		 System.out.println("Routing Number : "+routingNum);
 	 }
+	 
+	final void verifySecurity() {
+		
+		System.out.println("Identity Verified by Bank Policy");
+	} 
+	
+	BankAcc applyFee(double fee) {
+		
+		if(balance >= fee) {
+			
+			balance  -= fee ;
+			
+			System.out.println("Remainig Balance : "+balance);
+		}
+		
+		else{
+			
+			System.out.println("Not Enough balance !!");
+			
+		}
+		
+		return this ;
+	}
 	 
  }
 
+ class SavingAcc extends BankAcc{
+	 
+	 double interestRate ;
+	 
+	 public SavingAcc(String accHolder ,int balance ,double interestRate) {
+		 
+		 super(accHolder ,balance);
+		 this.interestRate = interestRate ;
+		 
+	 }
+	 
+	 @Override 
+	 public void displayInfo() {
+		 
+		 super.displayInfo();
+		 System.out.println("Interest rate : "+interestRate);
+	 }
+	 
+	void sendAuditor() {
+		
+		Auditor ad = new Auditor();
+		ad.checkOut(this);
+	}
+	 
+ }
+ 
+ class Auditor{
+	 
+	 void checkOut(SavingAcc acc) {
+		 
+		 System.out.println("Auditin account for : "+acc.accHolder);
+	 }
+ }
+ 
+ final class VIPsaveacc extends SavingAcc{ 
+	 
+	 public VIPsaveacc(String accHolder ,int balance ,double interestRate) {
+		 
+		 super(accHolder ,balance , interestRate);
+	 }
+ }
 public class Banker {
 
+	public static void main(String[] args) {
+		
+	BankAcc b1 = new BankAcc("Nanu");
+	b1.displayInfo();
+	
+	b1.applyFee(20).applyFee(30);
+	
+	SavingAcc s1 = new SavingAcc("Nanu", 500, 4.5);
+	
+	s1.displayInfo();
+	
+	s1.verifySecurity();
+	
+	s1.sendAuditor();
+	
+	VIPsaveacc vip = new VIPsaveacc("Gannu", 10000, 8.0);
+	vip.displayInfo();
+	
+	}
+	
 }
