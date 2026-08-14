28087c595477ba4916e61840d2d410f072fc96bf
digvijay parade <digvijayparade@gmail.com>
2026-08-14 16:41:57 +0530

    //

--- src/keywords/this_super_final/Cartel.java
+++ src/keywords/this_super_final/Cartel.java
@@ -7,8 +7,54 @@ abstract class Gadi{
 	final int maxRentalDays = 30 ;
 	static double totalFleetRevenue ;
 	
+	public Gadi(String brand ,double dailyRate ) {
+		
+		this.brand = brand ;
+		this.dailyRate = dailyRate ;
+	}
+	
+	public Gadi(String brand) {
+		
+		this(brand,50);
+	}
+	
+	abstract double calculateCost(int days);
+	
+	static void displayFleetStats() {
+		
+		System.out.println("Total Fleet Revenue : "+totalFleetRevenue);
+	}
+	
+	Gadi applyPromoDiscount(double discountPercentage) {
+		
+		this.dailyRate -= this.dailyRate*(discountPercentage/100.0);
+		
+		return this ;
+	}
+	
 }
 
-public class Cartel {
-
+class Car extends Gadi{
+	
+	boolean hasGPS ;
+	public Car(String brand ,double dailyRate ,boolean hasGPS) {
+		
+		super(brand ,dailyRate ) ;
+		this.hasGPS = hasGPS ;
+	}
+	
+	@Override
+	public double calculateCost(int days) {
+		
+		
+		return totalFleetRevenue;
+	}
 }
+
+public class Cartel{}
+
+
+
+
+
+
