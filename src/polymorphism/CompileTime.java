01b20a66a4f0d6d39406a926a32aaf3418f8accb
digvijay parade <digvijayparade@gmail.com>
2026-08-15 19:51:31 +0530

    //

---
+++ src/polymorphism/CompileTime.java
@@ -0,0 +1,56 @@
+package polymorphism ;
+
+class CombatEngine{
+	
+	String monsterName ;
+	int Health ;
+	boolean isAttacked ;
+	
+	public CombatEngine(String monsterName, int Health ,boolean isAttacked) {
+		
+		this.monsterName = monsterName ;
+		this.Health = Health ;
+		this.isAttacked = isAttacked ;
+		
+	}
+	
+	void attack(String mName) {
+		
+		if (isAttacked) {
+			
+			this.Health -= 40;
+			System.out.println("Monster Was Attacked now Health of it is "+this.Health);
+		}
+		
+		else {
+			
+			System.out.println(this.monsterName+" "+"Escaped !!");
+		}
+		
+	}
+	
+	void attack(int damage) {
+		
+		System.out.println("Special attack used ");
+		
+		this.Health -= damage ;
+		
+		System.out.println();
+		System.out.println("Monster Was Attacked now Health of it is "+this.Health);
+		
+	}
+}
+
+public class CompileTime{
+	
+	//compile time means overloading
+	public static void main(String[] args) {
+		
+		CombatEngine Nanu = new CombatEngine("Thanos",1000,true);
+		
+		Nanu.attack("Thanos");
+		Nanu.attack(100);
+		
+	}
+	
+}
