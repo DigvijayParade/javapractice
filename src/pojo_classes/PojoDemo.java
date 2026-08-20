5d17bbbbd816e6e6e8ec8933f0ebe888e53672d1
digvijay parade <digvijayparade@gmail.com>
2026-08-20 09:25:17 +0530

    /

--- src/pojo_classes/PojoDemo.java
+++ src/pojo_classes/PojoDemo.java
@@ -2,4 +2,37 @@ package pojo_classes;
 
 public class PojoDemo {
 
+	private String name ;
+	private int id ;
+	private String address ;
+	
+	public PojoDemo(String name,int id ) {
+		
+		if(!(name == null)) {
+			
+		this.name = name ;
+		
+		}
+		
+		if(id > 0 && ){}
+		this.id = id ;
+
+	}
+	
+	public void setAddress(String address) {
+		
+		if(!(address == null)) {
+			
+			this.address = address ;
+		}
+		else {
+			
+			System.out.println("the address should be filled !!");
+		}
+	}
+	
+	public String getAddress() {
+		
+		return this.address ;
+	}
 }
