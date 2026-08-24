ba51f4fe2da124901f267787320ad3cbf5cb8a14
digvijay parade <digvijayparade@gmail.com>
2026-08-24 08:30:13 +0530

    //

--- src/typecasting/CastingDemo.java
+++ src/typecasting/CastingDemo.java
@@ -1,4 +1,4 @@
-package Typecasting;
+package typecasting;
 
 public class CastingDemo {
 
--- src/typecasting/Parent.java
+++ src/typecasting/Parent.java
@@ -1,5 +1,59 @@
-package Typecasting;
+package typecasting ;
 
-public class ObjectCasting {
+import java.util.Objects;
 
+class Child extends Parent {
+
+	public Child(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		
+		Parent p = new Child("Nanu");
+		
+		Child c = (Child)p ;
+		
+		System.out.println(c);
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Name : "+name ;
+	}
+	
+	@Override
+	public int hashCode() {
+		
+		return Objects.hash(name);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		
+		if (this == o) return true;
+	    if (o == null || getClass() != o.getClass()) return false;
+	    
+	    Child other = (Child) o; 
+	    return Objects.equals(name, other.name);
+		
+	}
+	
+	
+}
+
+public class Parent {
+	
+	String name ;
+	
+	public Parent(String name) {
+		
+		this.name = name ;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
