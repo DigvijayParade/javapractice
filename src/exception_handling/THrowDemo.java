48ab45c09ab3a103822fbd9bba1a05a81a5efa4e
digvijay parade <digvijayparade@gmail.com>
2026-08-29 22:28:58 +0530

    ////

---
+++ src/exception_handling/THrowDemo.java
@@ -0,0 +1,33 @@
+package exception_handling;
+
+import java.io.FileInputStream;
+
+public class THrowDemo {
+
+	static void m1() throws Exception{
+		
+		FileInputStream file = new FileInputStream("C://info.txt");
+		System.out.println(file);
+		System.out.println("in the compiletime Exception The throws block is neccessary!!");
+	}
+	
+	static void m2() {
+		
+		System.out.println("in the runtime Exception The throws block is neccessary!! ");
+		int a = 10 ;
+		int b = 0 ;
+		System.out.println(a / b);
+	}
+	
+	public static void main(String[] args) {
+		
+		try {
+			m1();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
+		m2();
+	}
+
+}
