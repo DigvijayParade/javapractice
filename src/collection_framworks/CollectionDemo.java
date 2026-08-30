539272ebf2a9ec3036d7d3292dc1ccd822faebbd
digvijay parade <digvijayparade@gmail.com>
2026-08-30 23:36:06 +0530

    //

--- src/collection_framworks/CollectionDemo.java
+++ src/collection_framworks/CollectionDemo.java
@@ -16,14 +16,25 @@ public class CollectionDemo {
 		
 		ArrayList <CollectionDemo> list = new ArrayList <>();
 		
-		System.out.println(list.add(new CollectionDemo("WristBsnd",101)));
+		list.add(new CollectionDemo("WristBsnd",101));
 		list.add(new CollectionDemo("Car",102));
 		System.out.println(list.get(1));
+		
+		for (CollectionDemo o : list) {
+			
+			o.printInfo();
+		}
 	}
 	
 	void printInfo() {
 		
 		System.out.println("Item Name : "+this.itemName);
 		System.out.println("item Id : "+this.itemId);
+	}
+	
+	@Override
+	public String toString() {
+		return "Name : "+itemName+", id : "+itemId;
+		
 	}
 }
