153f4446ac90fd67956f4cd461b6896ffd3ed4e8
digvijay parade <digvijayparade@gmail.com>
2026-09-01 02:13:04 +0530

    //

---
+++ src/collection_framworks/ArrobjDe.java
@@ -0,0 +1,78 @@
+package collection_framworks;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Objects;
+
+public class ArrobjDe {
+
+	String name ;
+	int id ;
+	
+	public ArrobjDe(String name,int id) {
+		
+		this.name = name ;
+		this.id = id ;
+	}
+	
+	public static void main(String[] args) {
+		
+		ArrayList <ArrobjDe> info = new ArrayList <>();
+		
+		info.add(new ArrobjDe("Nanu", 101));
+		info.add(new ArrobjDe("Sonu", 102));
+		info.add(new ArrobjDe("Monu", 103));
+		info.add(new ArrobjDe("Ganu", 104));
+		info.add(new ArrobjDe("Nanu", 101));
+		
+		System.out.println("-----------OG list ------------");
+		for(ArrobjDe o : info) {
+			
+			System.out.println(o);
+		}
+		
+		Iterator <ArrobjDe> it = info.iterator();
+		
+		while(it.hasNext()) {
+			
+			ArrobjDe e = it.next();
+			if(e.name.equals("Nanu")) {
+				
+				it.remove();
+				break;
+			}
+			
+		}
+		
+		System.out.println("----------New List ----------");
+for(ArrobjDe o : info) {
+			
+			System.out.println(o);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Name : "+name+"||"+"ID : "+id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArrobjDe other = (ArrobjDe) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+	
+	
+}
