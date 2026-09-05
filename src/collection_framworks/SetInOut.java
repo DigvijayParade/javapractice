1b48571771cf8b12a5773942e2dd60a21586e561
digvijay parade <digvijayparade@gmail.com>
2026-09-06 01:18:43 +0530

    //

--- src/collection_framworks/SetInOut.java
+++ src/collection_framworks/SetInOut.java
@@ -4,16 +4,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetInOut {
+    public static void main(String[] args) {
+        
+        Set<StudentData> stdData = new HashSet<>();
 
-	public static void main(String[] args) {
-		
-		Set <StudentData> stdData = new HashSet<>(Set.of(new StudentData("nanu",101),
-				new StudentData("Swara", 102),
-				new StudentData("Nanu",101)));
-		
-		System.out.println(stdData.size());
-		System.out.println(stdData);
-		
-	
-	}
+        StudentData s1 = new StudentData("Nanu", 101);
+        StudentData s2 = new StudentData("Swara", 102);
+        StudentData s3 = new StudentData("Nanu", 101);
+
+        stdData.add(s1); // Step 1
+        stdData.add(s2); // Step 2
+        stdData.add(s3); // Step 3
+
+        System.out.println("Size: " + stdData.size()); // Output: 2
+    }
 }
