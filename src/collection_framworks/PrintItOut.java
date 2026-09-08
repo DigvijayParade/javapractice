f5c02011ec0caee1537f9888746a62d9f951e419
digvijay parade <digvijayparade@gmail.com>
2026-09-09 01:21:38 +0530

    //

---
+++ src/collection_framworks/PrintItOut.java
@@ -0,0 +1,19 @@
+package collection_framworks;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class PrintItOut {
+
+	public static void main(String[] args) {
+		
+		TreeMap <Integer,String> some = new TreeMap<>(Map.of(10,"Nanu",1,"Gyat"));
+		
+		for (Map.Entry<Integer, String> k : some.sequencedEntrySet()) {
+			
+//			System.out.println(k.getValue());
+//			System.out.println(k.getKey());
+			System.out.println(k.getKey()+"="+k.getValue());
+		}
+	}
+}
