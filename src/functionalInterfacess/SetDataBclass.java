e07f14758b93a927fd99eb352b5530a0c2c282f2
digvijay parade <digvijayparade@gmail.com>
2026-09-10 21:53:06 +0530

    Built in FIs

--- src/functionalInterfacess/SetDataBclass.java
+++ src/functionalInterfacess/SetDataBclass.java
@@ -10,16 +10,14 @@ public class SetDataBclass {
 		DataB s2 = new DataB("Sonu",102);
 		DataB s3 = new DataB("Monu",103);
 		
-		Function <DataB,Integer> f1 = (d) ->(d.getId());
-		Predicate <DataB> f2 = (d) -> (d.getName() == "Nanu");
-		Consumer <DataB> f3 = (d) -> System.out.println(new StringBuilder(d.getName()).reverse().toString());
-		Supplier<DataB> f4 = () -> new DataB("Durgesh", 10000);
+		Function <DataB,Integer> getId = obj -> obj.getId();
+		Predicate <DataB> checkName = obj -> obj.getName().equals("nanu");
+		Consumer <DataB> storeObj = obj ->{ System.out.println(  obj.getName());};
+		Supplier <DataB> giveObj = () -> new DataB("Gannu",104);
 		
-		System.out.println(f1.apply(s3));
-		System.out.println(f2.test(s1));  
-		f3.accept(s1);                   
-		
-		DataB newObj = f4.get();
-		System.out.println(newObj);
+		System.out.println(getId.apply(s3));
+		System.out.println(checkName.test(s3));
+							storeObj.accept(s3);
+		System.out.println(giveObj.get());
 	}
 }
