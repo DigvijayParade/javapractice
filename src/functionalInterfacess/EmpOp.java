2a7d7ca4ff97994e3de6f36bc2b26764c415ae09
digvijay parade <digvijayparade@gmail.com>
2026-09-16 23:02:11 +0530

    methods in the FI/Interface

--- src/functionalInterfacess/EmpOp.java
+++ src/functionalInterfacess/EmpOp.java
@@ -4,4 +4,15 @@ package functionalInterfacess;
 public interface EmpOp {
 
 	String apply (Employee emp);
+	
+	static void m1() {System.out.println("this is the static method");
+	
+		m2();
+	
+	};
+	
+	private static void m2() {
+		
+		System.out.println("We can call this private meth into the another methods of the interfae");
+	};
 }
