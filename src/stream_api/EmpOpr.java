aab9d63eb37a925b4d11288c09de9480deea8c6a
digvijay parade <digvijayparade@gmail.com>
2026-09-18 13:33:17 +0530

    //

--- src/stream_api/EmpOpr.java
+++ src/stream_api/EmpOpr.java
@@ -7,8 +7,6 @@ public class EmpOpr {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Employees> empData = new ArrayList<>();
-		
 		ArrayList<Employees> empList = new ArrayList<>(List.of(
 	            new Employees("Alex Johnson", 45, "Male", 95000.00),
 	            new Employees("Jane Smith", 34, "Female", 72000.00),
@@ -20,16 +18,28 @@ public class EmpOpr {
 	            new Employees("Jane Smith", 34, "Female", 72000.00) 
 	        ));
 		
-		List<String> femaleEmp = empList.stream()
-								.filter(e -> e.getGender().equalsIgnoreCase("Female"))
-								.map(e -> e.getName())
-								.toList();
+//		List<String> femaleEmp = empList.stream()
+//								.filter(e -> e.getGender().equalsIgnoreCase("Female"))
+//								.map(e -> e.getName())
+//								.toList();
+//		
+//		System.out.println("Female Employees ....");
+//		
+//		for (String f : femaleEmp) {
+//			
+//			System.out.println(f);
+//		}
 		
-		System.out.println("Female Employees ....");
+		int femalecount = 0 ;
 		
-		for (String f : femaleEmp) {
+		for (Employees e : empList) {
 			
-			System.out.println(f);
+			if(e.getGender().equalsIgnoreCase("Female")) {
+				
+				femalecount ++;
+			}
+			
 		}
+		System.out.println(femalecount);
 	}
 }
